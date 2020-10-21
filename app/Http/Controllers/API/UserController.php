d9664cbfa7f8f6dbec8c15b6cad621ffd76d8216
fanicahyo7 <fanidc7@gmail.com>
2020-10-21 20:32:26 +0700

    api food

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,9 +9,12 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
+
     public function login(Request $request){
         try{
             //validasi input
@@ -47,6 +50,85 @@ class UserController extends Controller
                 'message' => 'Something went wrong',
                 'error' => $error,
             ], 'Authentication Failed',500);
+        }
+    }
+
+    public function register(Request $request){
+        try {
+            $request->validate([
+                'name' => ['required','string','max:255'],
+                'email' => ['required','string','max:255','email','unique:users'],
+                'password' => $this->passwordRules()
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'phoneNumber' => $request->phoneNumber,
+                'houseNumber' => $request->houseNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'Authenticated');
+
+        }catch(Exception $error){
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed',500);
+        }
+    }
+
+    public function logout(Request $request){
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
+
+    public function fetch(Request $request){
+        return ResponseFormatter::success($request->user(),'Data user berhasil diambil');
+    }
+
+    public function updateProfile(Request $request){
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user,'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request){
+        $validator = Validator::make($request->all(),[
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if ($validator->fails()){
+            return ResponseFormatter::error([
+                ['error' => $validator->errors()],
+                'Update photo fails',
+                401
+            ]);
+        }
+
+        if($request->file('file')){
+            $file = $request->file->store('assets/user','public');
+
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file],'File successfully uploaded');
         }
     }
 }
