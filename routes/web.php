047bbd927b9afeadb831696962697001acec9a8b
fanicahyo7 <fanidc7@gmail.com>
2020-10-23 20:45:44 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+use function Clue\StreamFilter\fun;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::prefix('dashboard')
+    ->middleware(['auth:sanctum','admin'])
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
 
 
 //midtrans
