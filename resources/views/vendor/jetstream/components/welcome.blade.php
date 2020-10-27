78d82ddba3a7f004f436350b8a5113730bd5ec0d
fanidc7 <fanidc7@gmail.com>
2020-10-27 10:37:20 +0700

    crud user

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -16,7 +16,7 @@
     <div class="p-6 border-t border-gray-200">
         <div class="flex items-center">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://tailwindcss.com/">Users</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/users">Users</a></div>
         </div>
     </div>
 
