a9edbabd07d4fc10af6870fe4839a861782d7dce
fanidc7 <fanidc7@gmail.com>
2020-11-05 09:48:47 +0700

    hash

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Storage;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -66,5 +67,11 @@ class User extends Authenticatable
 
     public function getUpdatedAtAttribute($value){
         return Carbon::parse($value)->timestamp;
+    }
+
+    public function setPasswordAttribute($password){
+        if(!empty($password)){
+            $this->attributes['password'] = Hash::make($password);
+        }
     }
 }
